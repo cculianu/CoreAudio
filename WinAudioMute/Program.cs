3f7614522b30fa346abc280287ab073c81bbb0e3
Calin Culianu <calin.culianu@gmail.com>
2018-08-29 13:43:35 +0300

    Enabled printing to console even for windows app. Trying to use Environment.Exit to propagate exit code out properly

--- WinAudioMute/Program.cs
+++ WinAudioMute/Program.cs
@@ -9,15 +9,27 @@ namespace WinAudioMute
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
+        internal static void AttachConsoleForCommandLineMode()
+        {
+            //must call before any calls to Console.WriteLine()
+            AttachConsole(ATTACH_PARENT_PROCESS);
+        }
+
+
         private static MMDevice device;
 
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
             MMDeviceEnumerator denum = new MMDeviceEnumerator();
             // Print devs for testing
             if (args.Length > 0 && args[0] == "-p")
             {
                 MMDeviceCollection coll = denum.EnumerateAudioEndPoints(EDataFlow.eRender, DEVICE_STATE.DEVICE_STATE_ACTIVE);
+                AttachConsoleForCommandLineMode();
                 System.Console.WriteLine("Number of devs: {0}", coll.Count);
                 for (int i = 0; i < coll.Count; ++i)
                 {
@@ -27,14 +39,18 @@ namespace WinAudioMute
                     System.Console.WriteLine("    Muted?: {0}  MasterVol: {1}  MasterVolScalar: {2}", vol.Mute,
                         vol.MasterVolumeLevel, vol.MasterVolumeLevelScalar);
                 }
-                return 0;
+                Environment.Exit(0);
             }
             //
 
             device = denum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
 
             if (device == null)
-                return 1;
+            {
+                AttachConsoleForCommandLineMode();
+                System.Console.Error.WriteLine("ERROR: Could not get default audio endpoint (no sound device?)");
+                Environment.Exit(1);
+            }
 
             if (args.Length > 0 && args[0].ToLower() == "mute")
             { 
@@ -44,7 +60,7 @@ namespace WinAudioMute
             {
                 UnMute();
             }
-            return 0;
+            Environment.Exit(0);
         }
 
         private static void Mute()
